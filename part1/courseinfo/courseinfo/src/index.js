c684bac07638dd66d4fc6ef32cc50ddc858fce04
Nam Bui <bnnam@ymail.com>
2019-09-26 00:33:58 +0200

    exercise 1.1

--- part1/courseinfo/courseinfo/src/index.js
+++ part1/courseinfo/courseinfo/src/index.js
@@ -9,21 +9,49 @@ const App = () => {
     const exercises2 = 7
     const part3 = 'State of a component'
     const exercises3 = 14
+    const Header = (props) => {
+        return (
+            <div>
+                <h1>{props.course}</h1>
+            </div>
+        )
+    }
+    const Part = (props) => {
+        return (
+            <div>
+                {props.part} {props.exercises}
+            </div>
+        )
+    }
+
+
+    const Content = () => {
+        return (
+            <div>
+                <Part part={part1} exercises={exercises1} />
+                <Part part={part2} exercises={exercises2} />
+                <Part part={part3} exercises={exercises3} />
+            </div>
+        )
+    }
+    const Total = (props) => {
+        return (
+            <div>
+                <br></br>
+                Number of exercises = {props.totalnumber} 
+            </div>
+        )
+    }
+
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+            <Header course={course} />
+            <Content />
+            <Total totalnumber={exercises1 + exercises2 + exercises3} />
+
         </div>
+
     )
 }
 
