9d3c92097c35815ba77619a935cc7996be2043cd
Nam Bui <bnnam@ymail.com>
2019-10-07 16:21:40 +0200

    testing 1c

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,39 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-
-const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    }
-
+const App = (props) => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+  
     return (
+      <div>
         <div>
-            <h1>{course.name}</h1>
-            <p>{course.parts[0].name} {course.parts[0].exercises}</p>
-            <p>{course.parts[1].name} {course.parts[1].exercises}</p>
-            <p>{course.parts[2].name} {course.parts[2].exercises}</p>
-            <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+          {left}
+          <button onClick={() => setLeft(left + 1)}>
+            left
+          </button>
+          <button onClick={() => setRight(right + 1)}>
+            right
+          </button>
+          {right}
         </div>
-        
-
+      </div>
     )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+  }
+  
+  
+  
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
