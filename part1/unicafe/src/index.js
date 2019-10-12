b959560d6bc6205c20d221763821774b6001e90c
Nam Bui <bnnam@ymail.com>
2019-10-12 02:42:37 +0200

    begynte anecdotes

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,72 +1,26 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistics = (props) => {
-    const good = props.good
-    const neutral = props.neutral
-    const bad = props.bad
-    if (good + neutral + bad !== 0) {
-        return (
-            <div>
-                <h1>statistics</h1>
-                <Statistic text="good" value={good} />
-                <Statistic text="neutral" value={neutral} />
-                <Statistic text="bad" value={bad} />
-                <Statistic text="all" value={good + neutral + bad} />
-                <Statistic text="average" value={Math.round((good + (-1) * bad) / (good + neutral + bad)*10)/10} />
-                <Statistic text="positive" value={Math.round(good / (good + neutral + bad) * 100*10)/10} />
-            </div>
-        )
-    } else return (<div>
-        <h1>statistics</h1>
-        <p>No feedback is given</p></div>)
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  return (
+    <div>
+      {props.anecdotes[selected]}
+    </div>
+  )
 }
 
-const Statistic = (props) => {
-    const value = props.value
-    const text = props.text
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
-    if (text !== "positive") return (
-        <table>
-                <tbody>
-                    <tr>
-                        <td width="70px">{text}</td>
-                        <td width="70px">{value}</td>
-                    </tr>
-                </tbody>
-        </table>
-            );
-            else return (
-        <table>
-                <tbody>
-                    <tr>
-                        <td width="70px">{text}</td>
-                        <td width="70px">{value}%</td>
-                    </tr>
-                </tbody>
-            </table>
-            )
-        }
-        
-const App = () => {
-    // save clicks of each button to own state
-    const [good, setGood] = useState(0)
-            const [neutral, setNeutral] = useState(0)
-            const [bad, setBad] = useState(0)
-            return (
-        <div>
-                <h1>give feedback</h1>
-                <button onClick={() => setGood(good + 1)}> good </button>
-                <button onClick={() => setNeutral(neutral + 1)}> neutral </button>
-                <button onClick={() => setBad(bad + 1)}> bad </button>
-                <Statistics good={good} neutral={neutral} bad={bad} />
-
-
-
-            </div>
-            )
-        }
-        
-ReactDOM.render(<App />,
-                document.getElementById('root')
-            )
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
